2c49ccffbd7ae527f098fdc9bd011f0ce48a382c
Esme <33888773+Eloring@users.noreply.github.com>
2019-07-12 13:58:26 +0800

    add 'import build_model'

--- tftrt/examples/object_detection/__init__.py
+++ tftrt/examples/object_detection/__init__.py
@@ -15,5 +15,5 @@
 # limitations under the License.
 # =============================================================================
 
-from .object_detection import download_model, download_dataset, optimize_model, benchmark_model
+from .object_detection import download_model, download_dataset, optimize_model, benchmark_model, build_model
 from .test import test
